aa7b7d031310273cdcd03fbe885b16b186f7d104
Shirleyleeeee <xli86shirley@gmail.com>
2016-04-30 22:10:08 -0400

    make connection

--- FinalProject/src/Login/Login.java
+++ FinalProject/src/Login/Login.java
@@ -1,34 +1,31 @@
-
-
+package client;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileInputStream;
 import javax.swing.*;
+
+
  
 public class Login implements ActionListener {
-	/**
-	 * @author 1: Hang Zhao
-	 * @author 2: Yang Zhao
-	 * @author 3: Xiaoyi Li
-	 */
+
 	JFrame jf;
 	JTextField jtf;
 	JPasswordField jpf;
  
 	public Login() {
-		jf = new JFrame("Login");
+		jf = new JFrame("login");
 		jf.setLayout(new GridLayout(5, 1));
 		jf.add(new JPanel());
-		JLabel jl1 = new JLabel("    User ID£∫  ");
+		JLabel jl1 = new JLabel("usernameÔºö");
 		jtf = new JTextField(12);
 		JPanel jp1 = new JPanel();
 		jp1.add(jl1);
 		jp1.add(jtf);
 		jf.add(jp1);
  
-		JLabel jl2 = new JLabel("Password£∫ ");
+		JLabel jl2 = new JLabel("passwordÔºö ");
 		jpf = new JPasswordField(12);
 		JPanel jp2 = new JPanel();
 		jp2.add(jl2);
@@ -36,11 +33,11 @@ public class Login implements ActionListener {
 		jf.add(jp2);
  
 		JPanel jp3 = new JPanel();
-		JButton jb1 = new JButton("Register");
+		JButton jb1 = new JButton("register");
 		jb1.addActionListener(this);
-		JButton jb2 = new JButton("Login");
+		JButton jb2 = new JButton("login");
 		jb2.addActionListener(this);
-		JButton jb3 = new JButton("Cancel");
+		JButton jb3 = new JButton("cancel");
 		jb3.addActionListener(this);
 		jp3.add(jb1);
 		jp3.add(jb2);
@@ -61,24 +58,24 @@ public class Login implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String comm = e.getActionCommand();
-		if (comm.equals("Register")) {
+		if (comm.equals("register")) { 
 			jf.dispose();
 			new register();
-		} else if (comm.equals("Login")) {
+		} else if (comm.equals("login")) {
 			if ("".equals(jtf.getText())
 					|| "".equals(new String(jpf.getPassword()))
 					|| jpf.getPassword() == null) {
 				JOptionPane.showConfirmDialog(
-						jf, 
-						"Please input your User ID and Password£°", "Password or User ID is incorrect£°",
+						jf,
+						"username or password cannot be emptyÔºÅ!\nplease input againÔºÅ", "error",
 						JOptionPane.DEFAULT_OPTION);
 				jtf.setText(null);
 				jpf.setText(null);
 				jtf.requestFocus();
 			} else {
-				String s = jtf.getText() + "::" + new String(jpf.getPassword());
-				String name = jtf.getText() + "::";
-				File file = new File("C:/Users/hangz/Desktop/Login.txt");
+				String s = jtf.getText() + "&&" + new String(jpf.getPassword());
+				String name = jtf.getText() + "&&";
+				File file = new File("/Users/shirley_lee/Documents/workspace/Test/src/reg.txt");
 				try { 
 					FileInputStream fis = new FileInputStream(file);
 					String s1 = "";
@@ -92,10 +89,10 @@ public class Login implements ActionListener {
 					fis.close();
 					int i = s1.indexOf(name);
 					int j = s1.indexOf(s);
-					if (i == -1) {
+					if (i == -1) { 
 						JOptionPane.showConfirmDialog(
 								jf, 
-								"We can not recognize your User ID £°\n please input again, or register£°", "Incorrect",
+								"username is not exsit!\nplease register or login againÔºÅ", "error",
 								JOptionPane.ERROR_MESSAGE);
 						jtf.setText(null);
 						jpf.setText(null);
@@ -104,26 +101,29 @@ public class Login implements ActionListener {
 						if (j == -1) {
 							JOptionPane.showConfirmDialog(
 									jf, 
-									"The password is incorrect! \n Please input again!", "Incorrect",
+									"password is error!\nplease input the password againÔºÅ", "error",
 									JOptionPane.DEFAULT_OPTION);
 							jpf.setText(null);
 							jpf.requestFocus();
 						} else {
 							JOptionPane.showConfirmDialog(
-									jf,
-									"Success£°\n" + "User ID £∫ " + jtf.getText()
-											+ "\nPassword £∫ "
+									jf, 
+									"successÔºÅ\n" + "username Ôºö " + jtf.getText()
+											+ "\npassword Ôºö "
 											+ new String(jpf.getPassword()),
-									"Login Output", JOptionPane.DEFAULT_OPTION);
+									"result", JOptionPane.DEFAULT_OPTION);
+							jf.dispose();
+							 new MainForm().setVisible(true);
 						}
 					}
 				} catch (Exception e1) {
 					e1.printStackTrace();
 				}
 			}
-		} else if (comm.equals("Cancel")) {
+		} else if (comm.equals("cancel")) {
 			System.exit(0);
 		}
+ 
 	}
  
 }
